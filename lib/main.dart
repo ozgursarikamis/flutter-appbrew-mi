b988fe8dd36446fffcc3a45178a2cb564ca2aada
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-05-10 00:19:26 +0300

    SafeArea

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.teal, 
+        body: SafeArea(child: 
+          Container(
+            color: Colors.white,
+            child: Text("Hello")
+          )
+        ), 
       ),
-    ),
-  );
+    );
+  }
 }
