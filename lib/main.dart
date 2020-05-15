9ce124f3c2e92be853b51651bf90b34b2976ccae
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-05-16 02:19:41 +0300

    another custom font

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal, 
-        body: SafeArea(child: 
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start, // spaceEvenly
-            children: <Widget>[
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start, // spaceEvenly
+                children: <Widget>[
               CircleAvatar(
                 radius: 50,
                 backgroundColor: Colors.red,
                 backgroundImage: AssetImage('images/profile.png'),
               ),
-              Text("Özgür Sarıkamış", style: TextStyle(
-                fontFamily: "Pacifico",
-                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold
-              ))
-              ]
-          )
-        ), 
+              Text("Özgür METMANIS",
+                  style: TextStyle(
+                      fontFamily: "Pacifico",
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold)),
+                      Text("SOFTWARE DEVELOPER", style: TextStyle(
+                        fontFamily: "Source Sans Pro",
+                        color: Colors.teal.shade100,
+                        fontSize: 16,
+                        letterSpacing: 1.4,
+                        fontWeight: FontWeight.bold,
+                      ))
+            ])),
       ),
     );
   }
