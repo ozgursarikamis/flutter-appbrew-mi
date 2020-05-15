0af3ce2ff773166587824f83b7d86279aa95f66c
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-05-16 02:44:47 +0300

    basic design completed

--- lib/main.dart
+++ lib/main.dart
@@ -25,13 +25,58 @@ class MyApp extends StatelessWidget {
                       fontSize: 20,
                       color: Colors.white,
                       fontWeight: FontWeight.bold)),
-                      Text("SOFTWARE DEVELOPER", style: TextStyle(
-                        fontFamily: "Source Sans Pro",
-                        color: Colors.teal.shade100,
-                        fontSize: 16,
-                        letterSpacing: 1.4,
-                        fontWeight: FontWeight.bold,
-                      ))
+              Text("SOFTWARE DEVELOPER",
+                  style: TextStyle(
+                    fontFamily: "Source Sans Pro",
+                    color: Colors.teal.shade100,
+                    fontSize: 16,
+                    letterSpacing: 1.4,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 25,
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.phone, color: Colors.teal),
+                      SizedBox(width: 10),
+                      Text(
+                        '+90 542 498 20 95',
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Code Pro',
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ],
+                  )),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "ozgur@metmanis.com",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Code Pro',
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ])),
       ),
     );
