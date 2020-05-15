516e5246dfde7276de6aeb623bbc951752de9548
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-05-16 02:49:23 +0300

    using Padding with Cards

--- lib/main.dart
+++ lib/main.dart
@@ -33,48 +33,54 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 1.4,
                     fontWeight: FontWeight.bold,
                   )),
-              Container(
+              Card(
                   color: Colors.white,
                   margin: EdgeInsets.symmetric(
                     vertical: 10,
                     horizontal: 25,
                   ),
+                  // padding: EdgeInsets.all(10),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.phone, color: Colors.teal),
+                        SizedBox(width: 10),
+                        Text(
+                          '+90 542 498 20 95',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Code Pro',
+                            fontSize: 14.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )),
+              Card(
+                color: Colors.white,
+                // padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: Padding(
                   padding: EdgeInsets.all(10),
                   child: Row(
                     children: <Widget>[
-                      Icon(Icons.phone, color: Colors.teal),
-                      SizedBox(width: 10),
+                      Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
                       Text(
-                        '+90 542 498 20 95',
+                        "ozgur@metmanis.com",
                         style: TextStyle(
                           color: Colors.teal.shade900,
                           fontFamily: 'Source Code Pro',
-                          fontSize: 14.0,
                         ),
-                      ),
+                      )
                     ],
-                  )),
-              Container(
-                color: Colors.white,
-                padding: EdgeInsets.all(10.0),
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      "ozgur@metmanis.com",
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'Source Code Pro',
-                      ),
-                    )
-                  ],
+                  ),
                 ),
               ),
             ])),
