0ee9b60590a5455c1281f3e8c12cacf23127355b
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-05-16 03:03:52 +0300

    using ListTile

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,13 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 1.4,
                     fontWeight: FontWeight.bold,
                   )),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
               Card(
                   color: Colors.white,
                   margin: EdgeInsets.symmetric(
